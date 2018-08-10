b39a220edfc24b0bf5ad05368487f1d876856fcb
AlinaRakhimova <rr.alina@yandex.ru>
2018-08-10 15:12:39 +0300

    Lesson 1

---
+++ desktop/src/ru/rakhimova/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.rakhimova.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.rakhimova.stargame.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 512;
+		config.width = 1024;
+		new LwjglApplication(new StarGame(), config);
+	}
+}
