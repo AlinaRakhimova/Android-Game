7aada93af89f402a4b03f16a7e907f93a17b07ce
AlinaRakhimova <rr.alina@yandex.ru>
2018-08-19 14:19:51 +0300

    Lesson 4 Added stars on the game screen

--- core/src/ru/rakhimova/stargame/screen/GameScreen.java
+++ core/src/ru/rakhimova/stargame/screen/GameScreen.java
@@ -4,17 +4,22 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import ru.rakhimova.stargame.base.Base2DScreen;
 import ru.rakhimova.stargame.math.Rect;
 import ru.rakhimova.stargame.screen.sprites.Background;
+import ru.rakhimova.stargame.screen.sprites.Star;
 
 
 public class GameScreen extends Base2DScreen {
 
+    private static final int STAR_COUNT = 20;
     private Background background;
     private Texture bgTexture;
+    private TextureAtlas atlas;
+    private Star star[];
 
     public GameScreen(Game game) {
         super(game);
@@ -25,6 +30,11 @@ public class GameScreen extends Base2DScreen {
         super.show();
         bgTexture = new Texture("textures/bg.png");
         background = new Background(new TextureRegion(bgTexture));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        star = new Star[STAR_COUNT];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
+        }
     }
 
     @Override
@@ -41,10 +51,16 @@ public class GameScreen extends Base2DScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
         batch.end();
     }
 
     public void update(float delta) {
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(delta);
+        }
 
     }
 
@@ -60,12 +76,16 @@ public class GameScreen extends Base2DScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bgTexture.dispose();
+        atlas.dispose();
     }
 }
 
