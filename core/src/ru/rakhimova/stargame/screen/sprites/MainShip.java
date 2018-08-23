fb8c9505468c76c237ad9f7956c7dd030b3f1495
AlinaRakhimova <39618952+AlinaRakhimova@users.noreply.github.com>
2018-08-23 23:25:06 +0300

    Update MainShip.java

--- core/src/ru/rakhimova/stargame/screen/sprites/MainShip.java
+++ core/src/ru/rakhimova/stargame/screen/sprites/MainShip.java
@@ -1,61 +1,122 @@
 package ru.rakhimova.stargame.screen.sprites;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
-import ru.rakhimova.stargame.base.Sprite;
-import ru.rakhimova.stargame.math.Rect;
+import ru.geekbrains.stargame.base.Sprite;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.screen.pool.BulletPool;
 
-public class MainShip extends Sprite {
 
-    private final float STEP = 0.005f;
-    private Vector2 v = new Vector2(0, 0);
-    private Vector2 vDelta = new Vector2(0, 0);
-    private Rect worldBounds;
-    private TextureRegion region;
+public class MainShip extends Ship {
 
-    public MainShip(TextureAtlas atlas) {
-        regions = new TextureRegion[1];
-        region = atlas.findRegion("main_ship");
-        regions[0] = new TextureRegion(region,0,0,region.getRegionWidth()/2, region.getRegionHeight());
-        setHeightProportion(0.2f);
-    }
+    private static final float SHIP_HEIGHT = 0.15f;
+    private static final float BOTTOM_MARGIN = 0.05f;
 
-    @Override
-    public void draw(SpriteBatch batch) {
-        batch.draw(
-                regions[frame], // текущий регион
-                getLeft(), worldBounds.getBottom(), // точка отрисовки
-                halfWidth, halfHeight, // точка вращения
-                getWidth(), getHeight(), // ширина и высота
-                scale, scale, // масштаб по оси x и y
-                angle // угол вращения
-        );
-        pos.add(vDelta);
+    private Vector2 v0 = new Vector2(0.5f, 0.0f);
+
+    private boolean pressedLeft;
+    private boolean pressedRight;
+
+
+
+    public MainShip(TextureAtlas atlas, BulletPool bulletPool) {
+        super(atlas.findRegion("main_ship"), 1, 2,2);
+        setHeightProportion(SHIP_HEIGHT);
+        this.bulletRegion = atlas.findRegion("bulletMainShip");
+        this.bulletHeight = 0.01f;
+        this.bulletV.set(0, 0.5f);
+        this.bulletDamage = 1;
+        this.bulletPool = bulletPool;
     }
 
     @Override
     public void update(float delta) {
-        pos.add(vDelta);
-        checkAndHandleBounds();
-    }
-
-    protected void checkAndHandleBounds() {
-        if (getRight() > worldBounds.getRight()) vDelta.set(0, 0);
-        if (getLeft() < worldBounds.getLeft()) vDelta.set(0, 0);
+        pos.mulAdd(v, delta);
+        if (getRight() > worldBounds.getRight()) {
+            setRight(worldBounds.getRight());
+            stop();
+        }
+        if (getLeft() < worldBounds.getLeft()) {
+            setLeft(worldBounds.getLeft());
+            stop();
+        }
     }
 
     @Override
     public void resize(Rect worldBounds) {
-        this.worldBounds = worldBounds;
-        pos.add(vDelta);
+        super.resize(worldBounds);
+        setBottom(worldBounds.getBottom() + BOTTOM_MARGIN);
     }
 
-    public void changePosition(float i) {
-        float step = STEP * i;
-        v.set(pos.x + step, 0);
-        vDelta = v.sub(pos);
+    public void keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.A:
+            case Input.Keys.LEFT:
+                pressedLeft = true;
+                moveLeft();
+                break;
+            case Input.Keys.D:
+            case Input.Keys.RIGHT:
+                pressedRight = true;
+                moveRight();
+                break;
+            case Input.Keys.UP:
+                shoot();
+                break;
+        }
     }
+
+    public void keyUp(int keycode) {
+        switch (keycode) {
+            case Input.Keys.A:
+            case Input.Keys.LEFT:
+                pressedLeft = false;
+                if (pressedRight) {
+                    moveRight();
+                } else {
+                    stop();
+                }
+                break;
+            case Input.Keys.D:
+            case Input.Keys.RIGHT:
+                pressedRight = false;
+                if (pressedLeft) {
+                    moveLeft();
+                } else {
+                    stop();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        if (touch.x < worldBounds.pos.x) {
+            moveLeft();
+        } else {
+            moveRight();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        stop();
+        return false;
+    }
+
+    private void moveRight() {
+        v.set(v0);
+    }
+
+    private void moveLeft() {
+        v.set(v0).rotate(180);
+    }
+
+    private void stop() {
+        v.setZero();
+    }
+
 }
